c9938615d964a23be59b39dc0e676137dfae901c
ManbirK <72926520+ManbirK@users.noreply.github.com>
2023-01-02 17:21:34 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,8 @@
+
+	//Get "player1_name" using localStorage.getItem() function and assign it to player1_name
+
+
+	
+	//Get "player2_name" using localStorage.getItem() function and assign it to player2_name
+
+	
